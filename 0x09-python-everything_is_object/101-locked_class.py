d9f08ae655184f6be989a9ba4c505f6678e3fd30
Adnane-Bahili <AdnaneBahili2@gmail.com>
2023-07-05 01:10:06 +0000

    Project

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""defines a locked class"""
+class LockedClass:
+    """
+    prevents user from initiating new LockedClass attributes
+    for attributes that are other than "first_name"
+    """
+    __slots__ = ["first_name"]
