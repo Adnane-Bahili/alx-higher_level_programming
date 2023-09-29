6e98856d0500a03fd7f5058efaaa4f22612a41dd
Adnane-Bahili <AdnaneBahili2@gmail.com>
2023-09-29 00:30:15 +0000

    fix

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Displays the status code of the response
-#          when sending a 'GET' request to the specified 'URL'
+# Displays the status code of the response, when sending a 'GET' request to the specified 'URL'
 curl -s -o /dev/null -w "%{http_code}" "$1"
--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Sends a 'JSON' 'POST' request to the specifed 'URL' with a given JSON file
-# Displays the body of the response
+# Sends a 'JSON' 'POST' request to the specifed 'URL' with a given JSO file, displays the body of the response
 curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
