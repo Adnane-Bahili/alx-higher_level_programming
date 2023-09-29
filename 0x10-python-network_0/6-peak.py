04812ad2e8e4834f1ef05c3b9b59fe996c194d77
Adnane-Bahili <AdnaneBahili2@gmail.com>
2023-09-29 02:37:40 +0000

    Efficient peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,20 +6,6 @@ def find_peak(list_of_integers):
     """returns the peak"""
     if list_of_integers == []:
         return None
-
-    list_size = len(list_of_integers)
-
-    if list_size == 1:
+    if list_of_integers:
+        list_of_integers.sort(reverse=True)
         return list_of_integers[0]
-    elif list_size == 2:
-        return max(list_of_integers)
-
-    med = int(list_size / 2)
-    high = list_of_integers[med]
-
-    if high > list_of_integers[med - 1] and high > list_of_integers[med + 1]:
-        return high
-    elif high < list_of_integers[med - 1]:
-        return find_peak(list_of_integers[:med])
-    else:
-        return find_peak(list_of_integers[med + 1:])
